5f46038c507cd36c0aca293e9ba7cb2bc736fc2b
Liam Osler <osler.liam@gmail.com>
2022-11-03 20:58:27 -0300

    setup basic auth

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,59 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import netlifyAuth from '../netlifyAuth.js'
+
+
 export default function Home() {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
+
+  useEffect(() => {
+    let isCurrent = true
+    netlifyAuth.initialize((user) => {
+      if (isCurrent) {
+        setLoggedIn(!!user)
+      }
+    })
+
+    return () => {
+      isCurrent = false
+    }
+  }, [])
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user)
+    })
+  }
+
+
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header text={'Welcome to the Public Space™'} />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We are in a public space, for the people who aren't able to access the super fancy
+          members-only area. You hear snobbish laughter in the distance.
         </p>
+        {loggedIn ? (
+          <div>
+            You're logged in! Please do visit{' '}
+            <Link href="/protected">
+              <a>the special, members-only space.</a>
+            </Link>
+          </div>
+        ) : (
+          <button onClick={login}>Log in here to access the members-only area.</button>
+        )}
       </main>
 
       <Footer />
